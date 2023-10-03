bf8e00e00f1a331df6cdcf4df95e6ad6116f4338
emckellips <79339641+emckellips@users.noreply.github.com>
2023-10-02 21:15:32 -0500

    Added CSS sheet and js script

---
+++ script.js
@@ -0,0 +1,16 @@
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('nav ul');
+
+navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+});
